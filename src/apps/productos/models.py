933e66ce5aa2eb8ad6174a53af7f9fbeff19fdc5
Juan Manuel Quintana <juanmquintana@gmail.com>
2021-11-29 13:18:20 -0300

    Solucion al problema de importacion de modulos

--- src/apps/productos/models.py
+++ src/apps/productos/models.py
@@ -5,7 +5,6 @@ class Productos(models.Model):
     nombre = models.CharField(max_length=250)
     precio = models.DecimalField(max_digits=9, decimal_places=2)
     cantidad = models.IntegerField(default=0)
-    #cantidad = models.IntegerField(null=True) --> cuando necesite que por defecto el campo sea nulo
-
+   
     class Meta:
         db_table = 'productos'
--- src/apps/productos/views.py
+++ src/apps/productos/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+
 from .models import Productos
+
+def listar(request):
+    template_name = "productos/listar.html"
+    ctx = {
+        "nombre_usuario": "Juan Manuel",
+        "lista_productos": Productos.objects.all()
+    }
+    return render(request,template_name,ctx)
